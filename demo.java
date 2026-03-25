119dd80c86c15e682f192f0f30efa95e0e506a9c
Meenakshi-dev1 <meenakshissp346@gmail.com>
2026-03-25 21:25:46 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+public class demo {
+    public static double add(double a, double b)
+    {
+        double c = a + b;
+        return c;
+    }
+     public static void main(String[] args) {
+        double a = 10.5;
+        double b = 7.2;
+        double c = a + b;
+
+        System.out.println(c);
+    
+}
+}
