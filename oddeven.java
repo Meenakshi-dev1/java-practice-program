4fa804da3604cd4367d1465c7ab0f3f3dec80a21
Meenakshi-dev1 <meenakshissp346@gmail.com>
2026-03-25 21:27:56 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+public class oddeven {
+    public static void main(String[] args)
+    {
+        // hardcode the number here
+        int number=10;
+        if (number%2==0)
+        {
+            System.out.println(number+" is even");
+        }
+        else{
+            System.out.println(number+"is odd");
+        }
+    }
+    
+}
